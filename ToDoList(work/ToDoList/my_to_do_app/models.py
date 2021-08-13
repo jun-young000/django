6b5c9df403aedc9ce0d65ce992a3178a69635428
jun-young000 <taropoint1@naver.com>
2021-08-13 19:43:28 +0900

    장고
    장고

---
+++ ToDoList(work/ToDoList/my_to_do_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model) : # models.Model  을 상속받아 Django 모델 class 생성
+    # 컬럼 지정 - 기본키 생성안하면 자동 생성
+    content = models.CharField(max_length=255) # 교재 175쪽
+
+# 모델 수정시 아래 두 명령어를 반드시 진행하고 다음 코딩으로 넘어가야 함
+    # 나) 모델의
+    # 변경사항
+    # 추출
+    # python manage.py makemigrations
+
+    # 다) 변경된
+    # 모델
+    # 사항
+    # DB에
+    # 반영
+    # python  manage.py  migrate
